3574ba2a915eaea3d81c70eba6ec201888cdb4db
Dhinesh Palanisamy <84257275+dhineshaps@users.noreply.github.com>
2025-03-29 19:23:54 +0000

    int learn

---
+++ go_basics/cards_project/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var card string = "Ace of strings"
+	//card := "Ace of strings" //only during the initialization we have to use := for next stime we can use "="
+	//card = "new value"
+	card := newCard //to get return from the function
+	fmt.Println(card)
+	//fmt.Println(newCard()) //VALID
+}
+
+// bool - True pr False
+// string - "Hi" or " Hows it is going"
+//int - 0 , -10000,99999
+//float64 - 10.00001, 0.00009, -100.003
+
+func newCard() string { //need to specify the type of datatype it will return
+
+	return "Five of diamonds"
+}
